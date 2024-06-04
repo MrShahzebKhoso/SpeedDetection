97f84a2f388f1d6322ecc6864b904e2899ddf6bf
MrShahzebKhoso <108329155+MrShahzebKhoso@users.noreply.github.com>
2024-06-04 15:32:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ import cv2
 import numpy as np
 import time
 
-net = cv2.dnn.readNetFromCaffe('resources\\MobilNet_SSD_opencv-master\\MobileNetSSD_deploy.prototxt',
-                               'resources\\MobilNet_SSD_opencv-master\\MobileNetSSD_deploy.caffemodel')
+net = cv2.dnn.readNetFromCaffe('resources\\MobileNetSSD_deploy.prototxt',
+                               'resources\\MobileNetSSD_deploy.caffemodel')
 
 app = Flask(__name__)
 
 class VideoReader:
     def __init__(self, video_source='sample.mp4'):
         self.cap = cv2.VideoCapture(video_source)
-
-        # self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        # self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.interline = -1
         self.line1X = 100
         self.line2X = 200
@@ -35,7 +32,6 @@ class VideoReader:
 
     def detect_MN(self, image):
         resized_image = cv2.resize(image, (300, 300))
-        # resized_image = image
         (startX, startY, endX, endY) = (0, 0, 0, 0)
         blob = cv2.dnn.blobFromImage(resized_image, 0.007843, (300, 300), 127.5)
         net.setInput(blob)
@@ -44,7 +40,6 @@ class VideoReader:
             self.CalcSpeed = 0
 
             return image, startX, startY, endX, endY
-            # return resized_image, startX, startY, endX, endY
         else:    
             for i in range(detections.shape[2]):
                 confidence = detections[0, 0, i, 2]
@@ -55,69 +50,17 @@ class VideoReader:
                         (startX, startY, endX, endY) = box.astype("int")
                         image = cv2.rectangle(resized_image, (startX, startY), (endX, endY), (255), 2)
                         if(self.save_image):
-                            # cropped = resized_image[startX:endX, startY:endY]
                             cropped = resized_image[startY:endY,startX:endX]
                             cv2.imwrite(self.car_image_url+f"{self.CalcSpeed:.2f}"+ ".jpg", cropped)
                             self.save_image = False
-            # original_size_image = cv2.resize(resized_image, (image.shape[1], image.shape[0]))
             return image, startX, startY, endX, endY
-            
-            # return image, startX, startY, endX, endY
 
-    # def detect_MN(self, image):
-    #     resized_image = cv2.resize(image, (300, 300))
-    #     blob = cv2.dnn.blobFromImage(resized_image, 0.007843, (300, 300), 127.5)
-    #     net.setInput(blob)
-    #     detections = net.forward()
-    #     (startX, startY, endX, endY) = (0, 0, 0, 0)
-    #     for i in range(detections.shape[2]):
-    #         confidence = detections[0, 0, i, 2]
-    #         if confidence > 0.2:
-    #             class_id = int(detections[0, 0, i, 1])
-    #             if class_id == 7:
-    #                 box = detections[0, 0, i, 3:7] * np.array([300, 300, 300, 300])
-    #                 (startX, startY, endX, endY) = box.astype("int")
-    #                 resized_image = cv2.rectangle(resized_image, (startX, startY), (endX, endY), (255), 2)
-    #     original_size_image = cv2.resize(resized_image, (image.shape[1], image.shape[0]))
-    #     return original_size_image, startX, startY, endX, endY
     def process_frame(self, frame, startX, startY, endX, endY):
         if (self.vehicle_detected == True):
             cv2.line(frame, (self.line1X, 50), (self.line1X, 250), (0, 255, 0), 2)
             cv2.line(frame, (self.line2X, 50), (self.line2X, 250), (0, 0, 255), 2)
             self.CalcSpeed = 0
-
-        # if startX < self.line1X:
-        #     self.interline = -1
-        # if self.line1X <= startX <= self.line2X:
-        #     if self.interline == -1:
-        #         self.startTime = time.time()
-        #     self.interline = 0
-        # if startX < self.line1X and self.interline == 0:
-        #     self.endTime = time.time()
-        #     self.interline = 1
-
-        # temp = startX
-        
-        # if (self.interline==-1):
-        #     if(startX >= self.line1X and startX <= self.line2X):
-        #         self.startTime = time.time()
-        #         self.interline = 1
-        #     if(startX <= self.line2X and startX >= self.line1X ):
-        #         self.startTime = time.time()
-        #         self.interline = 1
-        
-        # if (self.interline==1):
-        #     if(startX >= self.line1X  and startX >= self.line2X):
-        #         self.endTime = time.time()
-        #         self.interline = 0
-        #     if(startX <= self.line2X  and startX >= self.line2X):
-        #         self.endTime = time.time()
-        #         self.interline = 0
-        # if (self.interline==0):
-        #     self.time = abs(self.startTime - self.endTime)
-        #     self.CalcSpeed = self.distance / self.time
-        #     self.interline = -1
-        
+       
         if not self.vehicle_detected:
             if self.line1X <= startX <= self.line2X:
                 self.startTime = time.time()
@@ -132,52 +75,8 @@ class VideoReader:
                 self.vehicle_detected = False
 
 
-
-
-        # Check if the vehicle crosses line 1
-        # if self.line1X <= startX <= self.line2X and self.interline == -1:
-        #     self.startTime = time.time()
-        #     self.interline = 0
-        #     print(self.startTime)
-
-        # # Check if the vehicle crosses line 2
-        # if startX > self.line2X and self.interline == 0:
-        #     self.endTime = time.time()
-        #     self.interline = 1
-        #     print(self.endTime)
-
-
-        # # Check if the vehicle crosses line 1 again in the opposite direction
-        # if startX < self.line1X and self.interline == 1:
-        #     self.startTime = time.time()
-        #     self.interline = 2
-        #     print(self.startTime)
-
-
-        # # Check if the vehicle crosses line 2 again in the opposite direction
-        # if self.line1X <= startX <= self.line2X and self.interline == 2:
-        #     self.endTime = time.time()
-        #     self.interline = 3
-        #     print(self.endTime)
-
-
-        # if self.endTime - self.startTime != 0 and self.interline == 1:
-        #     self.time = self.startTime - self.endTime 
-        #     self.CalcSpeed = self.distance / self.time
-
-        
-        # Calculate the speed
-        # if self.endTime != 0 and self.startTime != 0 and self.interline == 3:
-        #     self.time = abs(self.endTime - self.startTime)
-        #     if self.time != 0:
-        #         self.CalcSpeed = self.distance / self.time
-        #     self.interline = -1  # Reset the interline state after calculating speed
-
-
-        # ctext = "" + str(self.CalcSpeed) + " m/s"
         ctext = f"{self.CalcSpeed:.2f} kmph"
         cv2.putText(frame, ctext, self.position, self.font, self.font_scale, self.font_color, self.thickness)
-        print(ctext)
 
         return frame
 
